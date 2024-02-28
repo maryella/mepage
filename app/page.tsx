b601869951cbeead42cac7cb6dac9adc72476bc5
Mary Ella Hill <hillmaryella@gmail.com>
2024-02-28 14:40:56 -0500

    Updates, add resume

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import { SocialCard } from "./components/SocialCard";
 export default function Home() {
   return (
     <main className="flex flex-1 min-h-dvh flex-col justify-center items-center">
-      <div className="flex flex-col flex-1 max-w-[380px] m-4 my-8 sm:m-10 max-h-[600px] justify-between">
-        <div className="flex flex-col">
+      <div className="flex flex-col flex-1 max-w-[380px] m-4 my-8 sm:m-10 max-h-[600px] justify-center">
+        <div className="flex flex-col mb-28 mt-14">
           <p className={`${cookie.className} text-8xl text-center`}>
             mary ella
             <span className="hidden sm:inline-block">&nbsp;hill</span>
@@ -15,16 +15,13 @@ export default function Home() {
             SOFTWARE ENGINEER
           </p>
         </div>
-        <p className={`${opensans.className} text text-center mt-6 mb-12`}>
-          Full stack software engineer{" / "}
-          <span className="whitespace-nowrap">Front-end&nbsp;focus</span>
-        </p>
+
         <div className="flex flex-col justify-center">
           <div className="flex flex-row gap-x-4 justify-between">
             <SocialCard
               pathD="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 144-208 0c-35.3 0-64 28.7-64 64l0 144-48 0c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zM176 352l32 0c30.9 0 56 25.1 56 56s-25.1 56-56 56l-16 0 0 32c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48 0-80c0-8.8 7.2-16 16-16zm32 80c13.3 0 24-10.7 24-24s-10.7-24-24-24l-16 0 0 48 16 0zm96-80l32 0c26.5 0 48 21.5 48 48l0 64c0 26.5-21.5 48-48 48l-32 0c-8.8 0-16-7.2-16-16l0-128c0-8.8 7.2-16 16-16zm32 128c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-16 0 0 96 16 0zm80-112c0-8.8 7.2-16 16-16l48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 32 32 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-64 0-64z"
               name="Resume"
-              link="somenwere"
+              link="/mhill-resume.pdf"
             />
             <SocialCard
               pathD="M100.3 448H7.4V148.9h92.9zM53.8 108.1C24.1 108.1 0 83.5 0 53.8a53.8 53.8 0 0 1 107.6 0c0 29.7-24.1 54.3-53.8 54.3zM447.9 448h-92.7V302.4c0-34.7-.7-79.2-48.3-79.2-48.3 0-55.7 37.7-55.7 76.7V448h-92.8V148.9h89.1v40.8h1.3c12.4-23.5 42.7-48.3 87.9-48.3 94 0 111.3 61.9 111.3 142.3V448z"
@@ -38,7 +35,7 @@ export default function Home() {
             />
           </div>
           <a
-            className={`text-center ${sono.className} text-sm mt-4 hover:font-bold`}
+            className={`text-center ${sono.className} text-sm mt-8 hover:font-bold`}
             href="mailto: hillmaryella@gmail.com"
             title="Email hillmaryella@gmail.com through native email client"
           >
