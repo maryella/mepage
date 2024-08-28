ec22d3c00017dc1e35e0d04da45f4bc5e1e6f14f
Mary Ella Hill <hillmaryella@gmail.com>
2024-08-28 13:25:54 -0500

    Fix for mobile

--- app/components/SocialCard.tsx
+++ app/components/SocialCard.tsx
@@ -11,26 +11,28 @@ export function SocialCard({
   name: string;
 }) {
   return (
-    <a href={link} target="_blank" className="group first:-ml-4 last:-mr-4">
-      <div className="flex flex-1 py-2 flex-col items-center rounded-md p-4 group-hover:bg-[rgb(var(--foreground-rgb))]/90 transition duration-200 group-hover:shadow-md transition duration-200">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="32"
-          width="32"
-          viewBox="0 0 512 512"
-        >
-          {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
-          <path
-            d={pathD}
-            className="fill-[rgb(var(--foreground-rgb))] group-hover:fill-[rgb(var(--background-start-rgb))] transition duration-200"
-          />
-        </svg>
-        <p
-          className={`${sono.className} text-sm font-medium text-center mt-2 group-hover:text-[rgb(var(--background-start-rgb))] transition duration-200`}
-        >
-          {name}
-        </p>
-      </div>
-    </a>
+    <div className="flex">
+      <a href={link} target="_blank" className="group first:-ml-4 last:-mr-4">
+        <div className="flex flex-1 py-2 flex-col items-center rounded-md p-4 group-hover:bg-[rgb(var(--foreground-rgb))]/90 transition duration-200 group-hover:shadow-md transition duration-200">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="32"
+            width="32"
+            viewBox="0 0 512 512"
+          >
+            {/* <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--> */}
+            <path
+              d={pathD}
+              className="fill-[rgb(var(--foreground-rgb))] group-hover:fill-[rgb(var(--background-start-rgb))] transition duration-200"
+            />
+          </svg>
+          <p
+            className={`${sono.className} text-sm font-medium text-center mt-2 group-hover:text-[rgb(var(--background-start-rgb))] transition duration-200`}
+          >
+            {name}
+          </p>
+        </div>
+      </a>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import { SocialCard } from "./components/SocialCard";
 export default function Home() {
   return (
     <main className="flex flex-1 min-h-dvh flex-col justify-center">
-      <div className="flex flex-col flex-1 m-4 my-8 self-center justify-center sm:m-10 md:w-3/4 md:max-w-[1080px]">
+      <div className="flex flex-col flex-1 m-4 my-8 self-center justify-center md:w-3/4 md:max-w-[1080px]">
         <div className="flex flex-col mt-12 md:mt-0 mb-8">
-          <p className={`${sono.className} text-5xl`}>
+          <p className={`${sono.className} text-4xl md:text-5xl`}>
             hi! <wbr />
             i&apos;m mary&nbsp;ella.
           </p>
@@ -21,8 +21,7 @@ export default function Home() {
           </p>
           <p className={`${sono.className} text-3xl mt-5`}>ok, bye!</p>
         </div>
-
-        <div className="flex flex-row self-center gap-x-4 justify-between w-full md:w-2/3 px-4">
+        <div className="flex flex-grow justify-between gap-x-4 md:w-2/3 md:self-center px-4">
           <SocialCard
             pathD="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 144-208 0c-35.3 0-64 28.7-64 64l0 144-48 0c-35.3 0-64-28.7-64-64L0 64zm384 64l-128 0L256 0 384 128zM176 352l32 0c30.9 0 56 25.1 56 56s-25.1 56-56 56l-16 0 0 32c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-48 0-80c0-8.8 7.2-16 16-16zm32 80c13.3 0 24-10.7 24-24s-10.7-24-24-24l-16 0 0 48 16 0zm96-80l32 0c26.5 0 48 21.5 48 48l0 64c0 26.5-21.5 48-48 48l-32 0c-8.8 0-16-7.2-16-16l0-128c0-8.8 7.2-16 16-16zm32 128c8.8 0 16-7.2 16-16l0-64c0-8.8-7.2-16-16-16l-16 0 0 96 16 0zm80-112c0-8.8 7.2-16 16-16l48 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 32 32 0c8.8 0 16 7.2 16 16s-7.2 16-16 16l-32 0 0 48c0 8.8-7.2 16-16 16s-16-7.2-16-16l0-64 0-64z"
             name="Resume"
